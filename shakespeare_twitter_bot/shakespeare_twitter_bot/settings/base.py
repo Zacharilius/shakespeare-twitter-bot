f5e69056ce27bb1434daae2ee858da496e118ab0
Zachary Bensley <zabensley@gmail.com>
2016-02-27 11:07:22 -0800

    Setup login

--- shakespeare_twitter_bot/shakespeare_twitter_bot/settings/base.py
+++ shakespeare_twitter_bot/shakespeare_twitter_bot/settings/base.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',    
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',    
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
@@ -64,6 +71,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.i18n',
+                'django.core.context_processors.request',
             ],
         },
     },
@@ -125,3 +133,18 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+
+
+# Authentication
+
+AUTHENTICATION_BACKENDS = (
+    # Default backend -- used to login by username in Django admin
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_EMAIL_VERIFICATION = "none"
+SOCIALACCOUNT_QUERY_EMAIL = True
+LOGIN_REDIRECT_URL = "/"
--- shakespeare_twitter_bot/shakespeare_twitter_bot/urls.py
+++ shakespeare_twitter_bot/shakespeare_twitter_bot/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^$', shakespeare_twitter_bot, name='shakespeare_twitter_bot'),
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
         home_files, name='home-files'),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'i18n/', include('django.conf.urls.i18n')),    
     url(r'^admin/', admin.site.urls),
 ]
